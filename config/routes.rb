3f8b2b63a7a19d0c5dac7d4ca1c08114027b2d78
Daniel Ge <dange@seas.upenn.edu>
2013-01-18 23:06:33 -0500

    Create User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DoortagsRails::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  devise_for :users
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -39,20 +40,5 @@ DoortagsRails::Application.routes.draw do
   #     end
   #   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'static_pages#index'
 end
