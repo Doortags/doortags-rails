8b46602b77b7a96869b8de4e2e4c689405c07ce4
Dennis Sell <sell.dennis@gmail.com>
2013-01-19 05:26:17 -0500

    Added capability with Twilio

--- app/workers/twilio_request.rb
+++ app/workers/twilio_request.rb
@@ -1,6 +1,23 @@
 class TwilioRequest
   @queue = :twilio_queue
-  def self.perform()
+  def self.perform(user_id, tag_id, message, sender_number, name)
+  	user = User.find(user_id)
+  	tag = Tag.find(tag_id)
 
+  	account_sid = "ACe6eee82649316e9f918b80e7ed1a8693"
+  	auth_token = "f3cab3ce0871d20393d7a3cf37b1517b"
+  	client = Twilio::REST::Client.new account_sid, auth_token
+  	from = "+14848213139"
+
+  	number = ""
+  	if sender_number
+  		number =  " from phone number #{sender_number}"
+  	end
+
+  	client.account.sms.messages.create(
+  		:from => from,
+  		:to => user.phone,
+  		:body => "#{name} left message at #{tag.location}#{number}. Message: #{message}"
+  	)
   end
 end
