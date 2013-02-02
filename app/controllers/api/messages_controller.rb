7e568ca64cbdf1664c48ab5287ff36b81dc05654
Daniel Ge <dange@seas.upenn.edu>
2013-02-02 16:52:32 -0500

    Change SMS message format

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -24,13 +24,15 @@ class Api::MessagesController < Api::BaseController
       message = "nothing..."
     end
 
+    body = "#{name} rang your doorbell at #{location}: \"#{message}\""
+
     unless user.nil?
       @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
    
       @twilio_client.account.sms.messages.create(
         :from => "+1#{twilio_phone_number}",
         :to => number_to_send_to,
-        :body => "Hey, #{name} is at your #{location} and says \"#{message}\""
+        :body => body
       )	
 
       render :json => {
